069c180de823091d3a7a8d819eaec73ece0e4fb1
Corrinne W <corrinneworden@gmail.com>
2021-05-01 10:47:25 -0400

    homeroutes.js started

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,1 +1,82 @@
+const router = require('express').Router();
+const { User, Shelter, Pet } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+  try {
+    // Get all pets and join with their shelter data
+    const petData = await Pet.findAll({
+      include: [
+        {
+          model: Shelter,
+          attributes: ['name', 'email'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const allPets = petData.map((pet) => pet.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', { 
+      pets: 'allPets', 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/pet/:id', async (req, res) => {
+  try {
+    const petData = await Pet.findByPk(req.params.id, {
+        include: [
+            {
+              model: Shelter,
+              attributes: ['name', 'email'],
+            },
+          ],
+    });
+
+    const individualPet = petData.get({ plain: true });
+
+    res.render('individualPet', {
+      ...individualPet,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Use withAuth middleware to prevent access to route
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Pet }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('dashboard', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+
+  res.render('login');
+});
+
 module.exports = router;
