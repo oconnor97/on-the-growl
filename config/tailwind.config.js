3ed3d3d8fe1c87e0a961775ec0070df5447c77ee
Corrinne W <corrinneworden@gmail.com>
2021-04-30 22:22:50 -0400

    custom color palette added to tailwindcss.config.js. initialfile.handlebars done.

---
+++ config/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      primary: "#a1e8cc", //magic mint
+      secondary: '#eddfef', //pale purple pantone
+      alert: '#ef476f', //paradise pink
+      accent_dark: '#495159', //charcoal
+      accent_light: '#ffd275' //orange yellow crayola
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
