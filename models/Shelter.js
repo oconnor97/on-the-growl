cbdbc109e75eaf60ef43f309cd601d2455802a2d
Jake <jakech617@gmail.com>
2021-05-01 10:38:14 -0400

    updated shelter model

--- models/Shelter.js
+++ models/Shelter.js
@@ -1,1 +1,40 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class Shelter extends Model {}
+
+Shelter.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+        shelter_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        shelter_location: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        shelter_email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            },
+        },
+        sequelize,
+        freeTableName: true,
+        underscored: true,
+        modelName: 'comment'
+    },
+
+);
+
 module.exports = Shelter;
