967f758091309d952cca98c4de78dad30a4c1ea2
Jake <jakech617@gmail.com>
2021-05-01 10:46:36 -0400

    updated the pet model

--- models/Pet.js
+++ models/Pet.js
@@ -1,1 +1,41 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Pet extends Model {}
+
+Pet.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        pet_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        shelter_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'shelter',
+                key: 'id'
+            }
+        },
+        sequelize,
+        freeTableName: true,
+        underscored: true,
+        modelName: 'comment'
+    },
+);
+
 module.exports = Pet;
