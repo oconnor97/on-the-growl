e08b7bd77e774efbfc74fd6ecc005f83fbcca368
Michael O'Connor <oconnor97@aol.com>
2021-05-05 12:42:53 -0400

    creating and debugging a route to send the user model data to the front end to make an api call based on user information

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,38 +1,47 @@
 const findBtn = document.getElementById('findBtn');
-// const fetch = require('node-fetch');
+
 
 
 
 findBtn.addEventListener('click', async () => {
-    let data = await getToken();
-    let options = {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': data.token_type + ' ' + data.access_token,
-      },
-      mode: 'cors'
-    }
-    console.log(options)
-    fetch('https://api.petfinder.com/v2/animals', options).then(function (resp) {
-  
-      // Return the response as JSON
-      return resp.json();
-  
-    }).then(function (data) {
-  
-      // Log the API data
-      console.log('token', data);
-  
-    }).catch(function (err) {
-  
-      // Log any errors
-      console.log('something went wrong', err);
-  
-    });
-  });
+  let data = await getToken();
+  let userData = await getUserData();
+  let options = {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': data.token_type + ' ' + data.access_token,
+    },
+    mode: 'cors'
+  }
+  // let apiUrl = 'https://api.petfinder.com/v2/animals'
 
-  async function getToken() {
-    return new Promise((resolve, reject) => {
-       fetch('/token').then( (result) => resolve(result.json()) );
-    });
- }
+  console.log(userData)
+  fetch('https://api.petfinder.com/v2/animals', options).then(function (resp) {
+
+    // Return the response as JSON
+    return resp.json();
+
+  }).then(function (data) {
+
+    // Log the API data
+    console.log('token', data);
+
+  }).catch(function (err) {
+
+    // Log any errors
+    console.log('something went wrong', err);
+
+  });
+});
+
+async function getToken() {
+  return new Promise((resolve, reject) => {
+    fetch('/token').then((result) => resolve(result.json()));
+  });
+}
+
+async function getUserData() {
+  return new Promise((resolve, reject) => {
+    fetch('/userData').then((result) => resolve(result.json()));
+  });
+}
